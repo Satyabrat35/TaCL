806b86ed6011f2becdf25e1d65582210f47b2ad0
satyabrat35 <satyabrat35@gmail.com>
2023-04-20 10:59:49 -0400

    script file

--- pretrainmodel/train_model.sh
+++ pretrainmodel/train_model.sh
@@ -1,10 +1,20 @@
-CUDA_VISIBLE_DEVICES=0,1 python train.py\
+#!/bin/bash -l
+#SBATCH --output=/common/home/sb2311/PycharmProjects/TaCL/TaCL/pretrainmodel/logfile
+#SBATCH -o /common/home/sb2311/PycharmProjects/TaCL/TaCL/pretrainmodel/output.txt
+
+export PATH="$PATH:/koko/system/anaconda/bin"
+cd /common/home/sb2311/PycharmProjects/TaCL/TaCL/pretrainmodel
+source setup.sh
+source activate python39
+
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 python train.py\
     --language english\
     --model_name bert-base-uncased\
     --train_data ../pretrainingdata/english_wiki.txt\
     --gpus 2\
     --max_len 256\
-    --bsz_per_gpu 64\
+    --bsz_per_gpu 32\
     --grad_acc_steps 2\
     --eff_bsz 256\
     --learning_rate 1e-4\
